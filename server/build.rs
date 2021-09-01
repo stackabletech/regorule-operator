1a29cc4a677093d913507aa14a3468dcf2ec299a
Lars Francke <github@lars-francke.de>
2021-09-01 23:53:02 +0200

    Add CLI handling and built information diagnostics (#60)

---
+++ server/build.rs
@@ -0,0 +1,10 @@
+use stackable_operator::crd::CustomResourceExt;
+use stackable_regorule_crd::RegoRule;
+
+fn main() -> Result<(), stackable_operator::error::Error> {
+    built::write_built_file().expect("Failed to acquire build-time information");
+
+    RegoRule::write_yaml_schema("../deploy/crd/regorule.crd.yaml")?;
+
+    Ok(())
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,50 @@
-mod config;
-
-use crate::config::RegoRuleConfig;
-use stackable_config::ConfigBuilder;
+use clap::{crate_version, App, AppSettings, Arg, SubCommand};
 use stackable_operator::crd::CustomResourceExt;
-use stackable_operator::{client, error};
+use stackable_operator::{cli, client};
 use stackable_regorule_crd::RegoRule;
 use std::env;
-use std::ffi::OsString;
-use tracing::{error, info};
+use tracing::error;
+
+mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
 
 #[tokio::main]
-async fn main() -> Result<(), error::Error> {
+async fn main() -> Result<(), anyhow::Error> {
     stackable_operator::logging::initialize_logging("REGORULE_OPERATOR_LOG");
 
-    let config: RegoRuleConfig =
-        ConfigBuilder::build(env::args_os().collect::<Vec<OsString>>(), "CONFIG_FILE")
-            .expect("Error initializing Configuration!");
+    // Handle CLI arguments
+    let matches = App::new(built_info::PKG_DESCRIPTION)
+        .author("Stackable GmbH - info@stackable.de")
+        .about(built_info::PKG_DESCRIPTION)
+        .version(crate_version!())
+        .arg(cli::generate_productconfig_arg())
+        .subcommand(
+            SubCommand::with_name("crd")
+                .setting(AppSettings::ArgRequiredElseHelp)
+                .subcommand(cli::generate_crd_subcommand::<RegoRule>()),
+        )
+        .arg(Arg::with_name("port").default_value("3030").required(false).takes_value(true).help("The port on which the webserver will listen that serves the OPA management API"))
+        .get_matches();
 
-    info!("Starting Stackable Operator for OpenPolicyAgent Rego rules");
+    if let ("crd", Some(subcommand)) = matches.subcommand() {
+        if cli::handle_crd_subcommand::<RegoRule>(subcommand)? {
+            return Ok(());
+        }
+    }
+
+    // Safe unwrap because we define a default
+    let port = matches.value_of("port").unwrap().parse()?;
+
+    stackable_operator::utils::print_startup_string(
+        built_info::PKG_DESCRIPTION,
+        built_info::PKG_VERSION,
+        built_info::GIT_VERSION,
+        built_info::TARGET,
+        built_info::BUILT_TIME_UTC,
+        built_info::RUSTC_VERSION,
+    );
 
     let client = client::create_client(None).await?;
 
@@ -26,11 +53,10 @@ async fn main() -> Result<(), error::Error> {
             .await
     {
         error!("Required CRDs missing, aborting: {:?}", error);
-        return Err(error);
+        return Err(error.into());
     };
 
     let watch_namespace = stackable_operator::namespace::get_watch_namespace()?;
-    stackable_regorule_operator::run_reflector_and_server(client, watch_namespace, config.port)
-        .await;
+    stackable_regorule_operator::run_reflector_and_server(client, watch_namespace, port).await;
     Ok(())
 }
