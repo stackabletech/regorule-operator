71d6761384798c2c8dbd21d8cf1b5cac33f07f1f
Teo Klestrup Röijezon <teo@nullable.se>
2022-01-24 11:17:27 +0100

    Move Service into Helm chart

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -1,14 +1,9 @@
 mod controller;
 
-use anyhow::anyhow;
 use clap::Parser;
-use stackable_operator::builder::ObjectMetaBuilder;
 use stackable_operator::cli::Command;
-use stackable_operator::k8s_openapi::api::core::v1::{Service, ServicePort, ServiceSpec};
 use stackable_operator::kube::CustomResourceExt;
-use stackable_operator::labels::{APP_INSTANCE_LABEL, APP_NAME_LABEL};
-use stackable_regorule_crd::{RegoRule, APP_NAME, APP_PORT};
-use std::collections::BTreeMap;
+use stackable_regorule_crd::{RegoRule, APP_PORT};
 
 mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
@@ -43,47 +38,10 @@ async fn main() -> anyhow::Result<()> {
             ))
             .await?;
 
-            let cluster_ip = create_cluster_ip_service();
-            if let Err(err) = client.create(&cluster_ip).await {
-                return Err(anyhow!(
-                    "Could not create ClusterIP service: {}",
-                    err.to_string()
-                ));
-            }
-
             let watch_namespace = stackable_operator::namespace::get_watch_namespace()?;
             controller::run_reflector_and_server(client, watch_namespace, APP_PORT).await?;
         }
     };
 
     Ok(())
-}
-
-pub fn create_cluster_ip_service() -> Service {
-    let mut selector = BTreeMap::new();
-    selector.insert(APP_INSTANCE_LABEL.to_string(), APP_NAME.to_string());
-    selector.insert(APP_NAME_LABEL.to_string(), APP_NAME.to_string());
-
-    Service {
-        metadata: ObjectMetaBuilder::new()
-            .name(APP_NAME)
-            // TODO: namespace?
-            .namespace("default")
-            // TODO: What would be the ownerreference here?
-            //.ownerreference_from_resource(regorule, None, Some(true))
-            //.context(ObjectMissingMetadataForOwnerRef)?
-            .build(),
-        spec: Some(ServiceSpec {
-            ports: Some(vec![ServicePort {
-                name: Some("http".to_string()),
-                port: APP_PORT.into(),
-                protocol: Some("TCP".to_string()),
-                ..ServicePort::default()
-            }]),
-            selector: Some(selector),
-            type_: Some("ClusterIP".to_string()),
-            ..ServiceSpec::default()
-        }),
-        ..Service::default()
-    }
 }
